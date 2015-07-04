6a3df9173cdf1da59ddfd95af3860aa20ccfe10f
Evan Briones <erbriones@gmail.com>
2015-07-04 15:33:12 -0700

    update CHANGES and bump version v0.2.1

--- shapeshift/__init__.py
+++ shapeshift/__init__.py
@@ -8,7 +8,7 @@ from collections import namedtuple
 from .formatters import JSONFormatter, KeyValueFormatter
 
 version_info = namedtuple("version_info", ["major", "minor", "patch"])
-VERSION = version_info(0, 2, 0)
+VERSION = version_info(0, 2, 1)
 __version__ = "{0.major}.{0.minor}.{0.patch}".format(VERSION)
 
 __all__ = ["JSONFormatter", "KeyValueFormatter"]
