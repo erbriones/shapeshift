9de5ccb55a989c40813f068cd2d2f6882eaed741
Evan Briones <erbriones@gmail.com>
2015-07-05 13:21:39 -0700

    add additional metadata

--- shapeshift/__init__.py
+++ shapeshift/__init__.py
@@ -9,6 +9,11 @@ from .formatters import JSONFormatter, KeyValueFormatter
 
 version_info = namedtuple("version_info", ["major", "minor", "patch"])
 VERSION = version_info(0, 3, 1)
+
+__title__ = "shapeshift"
+__author__ = "Evan Briones"
+__copyright__ = "Copyright 2015, Evan Briones"
+__license__ = "MIT LICENSE"
 __version__ = "{0.major}.{0.minor}.{0.patch}".format(VERSION)
 
 __all__ = ["JSONFormatter", "KeyValueFormatter", "MessagePackFormatter"]
